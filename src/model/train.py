03c171f95a031db1eb1ea7b0551e1afaca3ca9f9
kvuong <kvuong@strongtie.com>
2025-04-03 04:02:00 +0000

    fixed test

--- src/model/train.py
+++ src/model/train.py
@@ -10,10 +10,12 @@ import mlflow.sklearn
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
+
 # define functions
 def main(args):
     # TO DO: enable autologging
     mlflow.sklearn.autolog()
+
     # read data
     df = get_csvs_df(args.training_data)
 
@@ -27,21 +29,43 @@ def main(args):
 def get_csvs_df(path):
     if not os.path.exists(path):
         raise RuntimeError(f"Cannot use non-existent path provided: {path}")
+
     csv_files = glob.glob(f"{path}/*.csv")
+
     if not csv_files:
         raise RuntimeError(f"No CSV files found in provided data path: {path}")
+
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
+
 def split_data(dataframe_df):
-    X, y = dataframe_df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, dataframe_df['Diabetic'].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    X = dataframe_df[
+        [
+            "Pregnancies",
+            "PlasmaGlucose",
+            "DiastolicBloodPressure",
+            "TricepsThickness",
+            "SerumInsulin",
+            "BMI",
+            "DiabetesPedigree",
+            "Age",
+        ]
+    ].values
+    y = dataframe_df["Diabetic"].values
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.30, random_state=0
+    )
+
     return X_train, X_test, y_train, y_test
+
+
 # TO DO: add function to split data
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    LogisticRegression(C=1 / reg_rate, solver="liblinear").fit(X_train, y_train)
 
 
 def parse_args():
@@ -49,16 +73,15 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     # add arguments
-    parser.add_argument("--training_data", dest='training_data',
-                        type=str)
-    parser.add_argument("--reg_rate", dest='reg_rate',
-                        type=float, default=0.01)
+    parser.add_argument("--training_data", dest="training_data", type=str)
+    parser.add_argument("--reg_rate", dest="reg_rate", type=float, default=0.01)
 
     # parse args
     args = parser.parse_args()
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
